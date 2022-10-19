20b83ed75d885febf91607991c077668bbe4eb35
marysgreenwood <marysgreenwood@gmail.com>
2022-10-18 23:47:27 -0400

    added save to local storage function

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
+    //retrieve date and time from moment.js
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-    var blockTime=parseInt ($("#hour-10").attr("data-time"))
-    console.log(blockTime)
-        //var currentHour = moment().hour();
-        /*$(".time-block").each (function() {
-            var blockTime=parseInt ($(this.attr("data-time"));
-            console.log(blockTime)
-            /*if (blockTime<currentHour){
-                //$(this>textarea).addClass("past")
-            })*/     
+    var currentHour = moment().hour();
+    //color-code time blocks
+   $(".time-block").each (function() {
+            var blockTime=parseInt ($(this).attr("data-time"));
+            if (blockTime<currentHour){
+                $("div>textarea").addClass("past")
+            }
+            else if (blockTime=currentHour){
+                $("div>textarea").addClass("present")
+            }
+            else {
+                $("div>textarea").addClass("future") 
+            }
+        }) 
         
+        //store appointments
+        $('.saveBtn').on('click', function () {
+            var appointments= $(this).siblings("textarea").val ()
+            localStorage.setItem("appointment", appointments)
+        })
     })
