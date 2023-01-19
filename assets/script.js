9b5c0b7f9c3be7611a880db8249adba388a6dfa9
marysgreenwood <marysgreenwood@gmail.com>
2023-01-19 13:36:39 -0500

    resolved color-coding issue  and saving to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
-$(document).ready(function(){
-    //retrieve date and time from moment.js
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-    var currentHour = moment().hour();
-    //color-code time blocks
-   $(".time-block").each (function() {
-            var blockTime=parseInt ($(this).attr("data-time"));
-            if (blockTime<currentHour){
-                $("div>textarea").addClass("past")
-            }
-            else if (blockTime=currentHour){
-                $("div>textarea").addClass("present")
-            }
-            else {
-                $("div>textarea").addClass("future") 
-            }
-        }) 
-        
-        //store appointments
-        $('.saveBtn').on('click', function () {
-            var appointments= $(this).siblings("textarea").val ()
-            localStorage.setItem("appointment", appointments)
-        })
-    })
+$(document).ready(function () {
+  //retrieve date and time from moment.js
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+  var currentHour = moment().hour();
+  console.log(currentHour);
+
+  //color-code time blocks
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("data-time"));
+    if (blockTime < currentHour) {
+      $(this).children("div>textarea").addClass("past");
+    } else if (blockTime === currentHour) {
+      $(this).children("div>textarea").addClass("present");
+    } else {
+      $(this).children("div>textarea").addClass("future");
+    }
+  });
+
+  //store appointments
+  $(".saveBtn").on("click", function () {
+    var appointments = $(this).siblings("textarea").val();
+    console.log(appointments);
+    localStorage.setItem("appointment", JSON.stringify(appointments));
+  });
+});
+
+//show saved appointments
+
