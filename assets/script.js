f89fb90aad578c586a4df6f21af82f58af1a6889
marysgreenwood <marysgreenwood@gmail.com>
2023-01-19 15:42:46 -0500

    added function to render saved appointments

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   //retrieve date and time from moment.js
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
   var currentHour = moment().hour();
-  console.log(currentHour);
 
   //color-code time blocks
   $(".time-block").each(function () {
@@ -16,13 +15,42 @@ $(document).ready(function () {
     }
   });
 
+  //array in which to store appointments
+
   //store appointments
   $(".saveBtn").on("click", function () {
-    var appointments = $(this).siblings("textarea").val();
-    console.log(appointments);
-    localStorage.setItem("appointment", JSON.stringify(appointments));
+    //debugger;
+    var appointments = JSON.parse(localStorage.getItem("appointments"));
+    var appmt = {
+      task: $(this).siblings("textarea").val(),
+      time: parseInt($(this).parent().attr("data-time")),
+    };
+    appointments.push(appmt);
+    localStorage.setItem("appointments", JSON.stringify(appointments));
   });
+
+  //show saved appointments
+  //update storage to save items into array
+  function showAppointments() {
+    var savedAppointments = JSON.parse(localStorage.getItem("appointments"));
+    if (!savedAppointments == []) {
+      $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("data-time"));
+        for (var i = 0; i < savedAppointments.length; i++) {
+          if (blockTime === savedAppointments[i].time) {
+            $(this).children("div>textarea").text(savedAppointments[i].task);
+          }
+        }
+      });
+    }
+  }
+  //loop through stored array to display items
+  //find div where attr("data-time") = appmnt time
+  //render appmt as inner html of div>text area
+  //console.log(savedAppointments);
+  //   savedAppointments.forEach( (appmt) => {
+  //     if (appmt.time=)
+  //   })
+
+  showAppointments();
 });
-
-//show saved appointments
-
